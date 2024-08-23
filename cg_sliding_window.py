c9f8af424ca0904d73e0d32ee6d6b1e5445aae04
nguyelea <30486885+nguyelea@users.noreply.github.com>
2024-08-23 09:42:12 -0700

    Add files via upload

--- cg_sliding_window.py
+++ cg_sliding_window.py
@@ -19,13 +19,12 @@ def sliding_window_analysis(seq, function, window_size=500,step_size=250):
             break
         yield start, end, function(seq[start:end])
 
-# Calculate CpG observed/expected ratios over sliding windows and return a dataframe with window start, end, and CpG obs/exp ratio
-sliding_window_df = pd.DataFrame()
+# Calculate CpG observed/expected ratios over sliding windows and return a list with window start, end, and CpG obs/exp ratio
+sliding_window_lst = []
 for start, end, cg in sliding_window_analysis(seq, obs_exp_ratio):
     row = (start, end, cg)
-    row_df = pd.DataFrame(row).transpose()
-    sliding_window_df = sliding_window_df.append(row_df)
-sliding_window_df.rename(columns = {0: "start", 1: "end", 2: "CpG_ratio"})
+    sliding_window_lst.append(row)
 
-# Export dataframe as a csv file
+# Convert list into dataframe and export as csv file
+sliding_window_df = pd.DataFrame(sliding_window_lst, columns = ['start', 'end', 'CpG_ratio'])
 sliding_window_df.to_csv('./sliding_window_out.csv', index = False, header = ['window_start', 'window_end', 'CpG_ratio'])
